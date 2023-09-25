206b24f01d43505847054af1d8e25d11c904b198
Kyuyeon Kim <kyukim@Kyuyeonui-MacBookPro.local>
2023-09-25 01:03:46 -0400

    Modified insert initial data

--- fusus/user/management/commands/insert_initial_data.py
+++ fusus/user/management/commands/insert_initial_data.py
@@ -10,13 +10,13 @@ class Command(BaseCommand):
         org1 = Organization.objects.create(name='Organization1', phone='123456789', address='Address1')
         org2 = Organization.objects.create(name='Organization2', phone='987654321', address='Address2')
 
-        for user_type, prefix in [('ADMIN', 'admin'), ('VIEWER', 'viewer'), ('USER', 'user')]:
+        for user_type in ['ADMIN', 'VIEWER', 'USER']:
             for idx, org in enumerate([org1, org2], 1):
                 # Create 2 users
                 for i in range(1, 3):
                     User.objects.create_user(
-                        email=f'{prefix}{(idx - 1) * 2 + i}@example.com',
-                        name=f'{prefix.capitalize()} User {(idx - 1) * 2 + i}',
+                        email=f'{user_type.lower()}{(idx - 1) * 2 + i}@example.com',
+                        name=f'{user_type} User {(idx - 1) * 2 + i}',
                         organization_id=org.id,
                         birthdate='1990-01-01',
                         user_type=user_type,
